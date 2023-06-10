d08efacf057a693f0a0453cc4ad3c69a6b5ff824
IgorBrave <135931654+IgorBrave@users.noreply.github.com>
2023-06-10 20:28:59 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === ('Природа.jpg') ){
-        myImage.setAttribute ('src','Тайга.jpg');
+    if(mySrc === ('Фото/Природа.jpg') ){
+        myImage.setAttribute ('src','Фото/Тайга.jpg');
     } else { 
-        myImage.setAttribute ('src','Лена.jpeg');
+        myImage.setAttribute ('src','Фото/Лена.jpeg');
     }
-    if(mySrc === ('Лена.jpeg') ){
-        myImage.setAttribute ('src','Природа.jpg');
+    if(mySrc === ('Фото/Лена.jpeg') ){
+        myImage.setAttribute ('src','Фото/Природа.jpg');
     }
 }
 var myButton = document.querySelector('button')
@@ -16,13 +16,13 @@ var myHeading = document.querySelector('h2')
 function setUserName() {
     var myName = prompt('Введите своё имя');
     localStorage.setItem('name', myName);
-    myHeading.textContent = 'Добро пожаловать ' + myName;
+    myHeading.textContent = 'Добро пожаловать на этот сайт, ' + myName;
 }
 if(! localStorage.getItem('name')){
     setUserName();
 } else {
     var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Добро пожаловать ' + storedName + ' на мой сайт';
+    myHeading.textContent = 'Добро пожаловать на этот сайт, ' + storedName ;
 }
 myButton.onclick = function() {
     setUserName();
